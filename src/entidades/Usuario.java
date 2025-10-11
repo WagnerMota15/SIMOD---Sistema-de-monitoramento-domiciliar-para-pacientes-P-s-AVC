e5b2e20bfdc652f8f0790efe7ffb822fab0eaf48
wendell yan <wendellyan1503@gmail.com>
2025-10-11 14:38:04 -0300

    Atualização da classe abstrata Usuário

--- src/entidades/Usuario.java
+++ src/entidades/Usuario.java
@@ -2,50 +2,82 @@ package entidades;
 
 public abstract class Usuario {
 	
-	protected Integer CPF;
+	protected String CPF;
 	protected String nomeCompleto;
 	protected String email;
 	protected String senha;
 	protected String numeroTelefone;
-
+	protected int id;
 	
-	public Usuario(Integer CPF, String nomeCompleto, String email, String senha, String numeroTelefone) {
-		this.CPF = CPF;
+	
+	public Usuario(String cPF, String nomeCompleto, String email, String senha, String numeroTelefone, int id) {
+		CPF = cPF;
 		this.nomeCompleto = nomeCompleto;
 		this.email = email;
 		this.senha = senha;
 		this.numeroTelefone = numeroTelefone;
+		this.id = id;
 	}
-	public Integer getCPF() {
+
+
+	public String getCPF() {
 		return CPF;
 	}
-	public void setCPF(Integer cPF) {
+
+
+	public void setCPF(String cPF) {
 		CPF = cPF;
 	}
+
+
 	public String getNomeCompleto() {
 		return nomeCompleto;
 	}
+
+
 	public void setNomeCompleto(String nomeCompleto) {
 		this.nomeCompleto = nomeCompleto;
 	}
+
+
 	public String getEmail() {
 		return email;
 	}
+
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+
 	public String getSenha() {
 		return senha;
 	}
+
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
+
 	public String getNumeroTelefone() {
 		return numeroTelefone;
 	}
+
+
 	public void setNumeroTelefone(String numeroTelefone) {
 		this.numeroTelefone = numeroTelefone;
 	}
-	
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	
 }

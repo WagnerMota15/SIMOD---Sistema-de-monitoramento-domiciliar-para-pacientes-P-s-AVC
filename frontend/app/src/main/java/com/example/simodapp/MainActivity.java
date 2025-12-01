996fbcbca8e776a1d685d7bb7397472f5da0ea74
´´Wagner <wagnergamesbrasil@gmail.com>
2025-12-01 10:43:49 -0300

    Dando prosseguimento com a integração do front end com o back end para a tela de login

--- frontend/app/src/main/java/com/example/simodapp/MainActivity.java
+++ frontend/app/src/main/java/com/example/simodapp/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -15,6 +14,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.simodapp.login.LoginActivity;
 
 import retrofit2.Call;
 import retrofit2.Callback;

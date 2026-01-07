b41d5a546f4588b9e6935e6343a4affe68e7555c
Wendell Yan <wendellyan1503@gmail.com>
2026-01-07 15:31:20 -0300

    chore: atualização e criação das estruturas de service,controllers e respository do paciente

---
+++ backend/src/main/java/com/SIMOD/SIMOD/services/PatientService.java
@@ -0,0 +1,25 @@
+package com.SIMOD.SIMOD.services;
+
+import com.SIMOD.SIMOD.domain.paciente.Patient;
+import com.SIMOD.SIMOD.dto.PatientRequest;
+import com.SIMOD.SIMOD.repositories.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class PatientService {
+
+    @Autowired
+    public PatientRepository patientRepository;
+
+    public Patient criarPaciente(PatientRequest dado){
+
+        Patient novoPaciente = new Patient();
+        novoPaciente.setNomeComplete(dado.nomeComplete());
+        novoPaciente.setEmail(dado.email());
+        novoPaciente.setCPF(dado.CPF());
+        novoPaciente.setPassword(dado.password());
+        novoPaciente.setTelephone(dado.telephone());
+        novoPaciente.setTipoAVC(dado.tipoAVC());
+        return patientRepository.save(novoPaciente);
+
+    }
+}

51cf8efc3efee3696859c53ca602f008b39098a1
´´Wagner <wagnergamesbrasil@gmail.com>
2026-01-17 11:27:53 -0300

    feat: implementação completa do back-end da tela de login. Passando como login o cpf ou email do usuário e sua senha.

--- backend/src/main/java/com/SIMOD/SIMOD/dto/auth/LoginRequest.java
+++ backend/src/main/java/com/SIMOD/SIMOD/dto/auth/LoginRequest.java
@@ -1,4 +1,9 @@
 package com.SIMOD.SIMOD.dto.auth;
 
-public record LoginRequest() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.br.CPF;
+
+public record LoginRequest(@NotBlank String login,@NotBlank @Size(min = 8) String password) {
 }
